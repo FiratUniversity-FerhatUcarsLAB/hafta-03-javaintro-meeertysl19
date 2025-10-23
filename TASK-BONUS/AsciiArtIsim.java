98a705bd54f52d0a88c35c1a9819f1c6fc8fcf1e
meeertysl19 <250542017@firat.edu.tr>
2025-10-23 19:07:06 +0300

    Update AsciiArtIsim.java

---
+++ TASK-BONUS/AsciiArtIsim.java
@@ -0,0 +1,9 @@
+public class AsciiIsim {
+    public static void main(String[] args) {
+        System.out.println("M     M  EEEEE  RRRR   TTTTT");
+        System.out.println("MM   MM  E      R   R    T  ");
+        System.out.println("M M M M  EEEE   RRRR     T  ");
+        System.out.println("M  M  M  E      R  R     T  ");
+        System.out.println("M     M  EEEEE  R   R    T  ");
+    }
+}
