47b6c90e4831643ec8217b52cc638ff50d85e69d
meeertysl19 <250542017@firat.edu.tr>
2025-10-23 19:01:05 +0300

    Update DikdortgenHesap.java

---
+++ Task-3-GeometrikHesaplamalar/DikdortgenHesap.java
@@ -0,0 +1,13 @@
+public class Dikdortgen {
+    public static void main(String[] args) {
+        double en = 4.5;
+        double boy = 7.9;
+
+        double alan = en * boy;
+        double cevre = 2 * (en + boy);
+
+        System.out.printf("Dikdörtgen En: %.2f, Boy: %.2f%n", en, boy);
+        System.out.printf("Alan = %.4f%n", alan);
+        System.out.printf("Çevre = %.4f%n", cevre);
+    }
+}
