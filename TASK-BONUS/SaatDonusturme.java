41fd2e5f0fb3bdfc27d57398833427608d6d4f85
meeertysl19 <250542017@firat.edu.tr>
2025-10-23 19:06:14 +0300

    Update SaatDonusturme.java

---
+++ TASK-BONUS/SaatDonusturme.java
@@ -0,0 +1,11 @@
+public class SaatDonusturme {
+    public static void main(String[] args) {
+        int toplamSaniye = 3665;
+
+        int saat = toplamSaniye / 3600;
+        int dakika = (toplamSaniye % 3600) / 60;
+        int saniye = toplamSaniye % 60;
+
+        System.out.printf("%d saniye = %d:%02d:%02d%n", toplamSaniye, saat, dakika, saniye);
+    }
+}
