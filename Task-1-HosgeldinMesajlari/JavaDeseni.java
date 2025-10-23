a10659959dbf335900fd7ced44f481fb727ef237
meeertysl19 <250542017@firat.edu.tr>
2025-10-23 18:54:57 +0300

    Update JavaDeseni.java

---
+++ Task-1-HosgeldinMesajlari/JavaDeseni.java
@@ -0,0 +1,13 @@
+public class ödev3 {
+   public static void main(String[] args) {
+    System.out.println("  JJJJJ    AAAAA   V       V   AAAAA");
+        System.out.println("    J     A     A   V     V   A     A");
+        System.out.println("    J     A     A    V   V    A     A");
+        System.out.println("    J     AAAAAAA     V V     AAAAAAA");
+        System.out.println("J   J     A     A      V      A     A");
+        System.out.println(" JJJ      A     A      V      A     A");
+    }
+     
+     
+    
+}
