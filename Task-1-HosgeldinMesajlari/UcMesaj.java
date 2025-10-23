27da2356be94f9297086873d7a5bc38d5d8db567
meeertysl19 <250542017@firat.edu.tr>
2025-10-23 18:52:52 +0300

    Update UcMesaj.java

---
+++ Task-1-HosgeldinMesajlari/UcMesaj.java
@@ -0,0 +1,10 @@
+public class ödev1 {
+   public static void main(String[] args) {
+       System.out.println("Java’ya Hoş Geldiniz");
+        System.out.println("Bilgisayar Bilimine Hoş Geldiniz");
+        System.out.println("Programlama Eğlencelidir");
+    }
+     
+     
+    
+}
