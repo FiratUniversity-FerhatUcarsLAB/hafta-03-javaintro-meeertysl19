bd84c46ec2ca66de4f5cd3062c19131eddf83a91
meeertysl19 <250542017@firat.edu.tr>
2025-10-23 19:04:10 +0300

    Update CarpimTablosu.java

---
+++ Task-5-TabloDesenleri/CarpimTablosu.java
@@ -0,0 +1,15 @@
+public class CarpimTablosu {
+    public static void main(String[] args) {
+        int sayi = 2;
+
+        System.out.println("-----------------");
+        System.out.println("2'nin Çarpım Tablosu");
+        System.out.println("-----------------");
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.printf("%d x %d = %d%n", sayi, i, sayi * i);
+        }
+
+        System.out.println("-----------------");
+    }
+}
