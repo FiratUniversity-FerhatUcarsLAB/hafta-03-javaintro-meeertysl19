079156dbd811e3e2cf4b89c7a5693a4cc74ba3cb
meeertysl19 <250542017@firat.edu.tr>
2025-10-23 18:54:18 +0300

    Update BesKereTekrar.java

---
+++ Task-1-HosgeldinMesajlari/BesKereTekrar.java
@@ -0,0 +1,12 @@
+public class ödev2 {
+   public static void main(String[] args) {
+       System.out.println("Java'ya Hoş geldiniz");
+        System.out.println("Java'ya Hoş geldiniz");
+        System.out.println("Java'ya Hoş geldiniz");
+         System.out.println("Java'ya Hoş geldiniz"); 
+         System.out.println("Java'ya Hoş geldiniz");
+    }
+     
+     
+    
+}
